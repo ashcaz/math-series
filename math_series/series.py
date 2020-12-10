b0c0fca3df869b32fa32700d0a6059cf1abe07ee
Ashley Casimir <ashcas@me.com>
2020-12-09 20:02:00 -0800

    lucas loop complete tests pass

--- math_series/series.py
+++ math_series/series.py
@@ -8,3 +8,14 @@ def fibonacci(n):
       fibonacci_list.append(fibonacci_sum)
     
     return fibonacci_list[n]
+
+def lucas(n):
+  lucas_list = [2,1]
+  if n < 2:
+    return lucas_list[n]
+  else:
+    for x in range(2,(n + 1)):
+      lucas_sum = lucas_list[x-1]+ lucas_list[x-2]
+      lucas_list.append(lucas_sum)
+
+  return lucas_list[n]
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,4 +1,5 @@
-from math_series.series import fibonacci
+from math_series.series import fibonacci, lucas
+
 
 def test_fibonacci_zero():
   actual = fibonacci(0)
@@ -18,4 +19,24 @@ def test_fibonacci_four():
 def test_fibonacci_twenty():
   actual = fibonacci(20)
   expected = 6765
+  assert actual == expected
+
+def test_lucas_zero():
+  actual = lucas(0)
+  expected = 2
+  assert actual == expected
+
+def test_lucas_one():
+  actual = lucas(1)
+  expected = 1
+  assert actual == expected
+
+def test_lucas_five():
+  actual = lucas(5)
+  expected = 11
+  assert actual == expected
+
+def test_lucas_forty_four():
+  actual = lucas(44)
+  expected = 1568397607
   assert actual == expected
